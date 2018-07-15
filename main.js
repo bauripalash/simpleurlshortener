7bd04309ae83f3777a2f3cf106c9ce5d25cfd0f0
Palash Bauri <palashbauri1@gmail.com>
2018-07-15 10:47:36 +0530

    added files

---
+++ main.js
@@ -0,0 +1,58 @@
+var endpoint = "https://www.jsonstore.io/8ba4fd855086288421f770482e372ccb5a05d906269a34da5884f39eed0418a1";
+
+function geturl(){
+    var url = document.getElementById("urlinput").value;
+    var protocol_ok = url.startsWith("http://") || url.startsWith("https://") || url.startsWith("ftp://");
+    if(!protocol_ok){
+        newurl = "http://"+url;
+        return newurl;
+        }else{
+            return url;
+        }
+}
+
+function getrandom() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 5; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+}
+
+function genhash(){
+    if (window.location.hash == ""){
+        window.location.hash = getrandom();
+    }
+}
+
+function send_request(url) {
+    this.url = url;
+    $.ajax({
+        'url': endpoint + "/" + window.location.hash.substr(1),
+        'type': 'POST',
+        'data': JSON.stringify(this.url),
+        'dataType': 'json',
+        'contentType': 'application/json; charset=utf-8'
+})
+}
+
+function shorturl(){
+    var longurl = geturl();
+    genhash();
+    send_request(longurl);
+}
+
+var hashh = window.location.hash.substr(1)
+
+if (window.location.hash != "") {
+    $.getJSON(endpoint + "/" + hashh, function (data) {
+        data = data["result"];
+
+        if (data != null) {
+            window.location.href = data;
+        }
+
+    });
+}
+
